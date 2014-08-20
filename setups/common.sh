27695cc55a309f8ba10b404009ac278448669ce4
Vaidas Mikalauskas <vaidas.mikalauskas@gmai.com>
2014-08-20 22:27:01 +0300

    upd

---
+++ setups/common.sh
@@ -0,0 +1,7 @@
+
+##############################################
+#                     GIT                    #
+##############################################
+git config --global user.name "Vaidas Mikalauskas"
+git config --global user.email vaidas.mikalauskas@gmail.com
+git config --global push.default simple
