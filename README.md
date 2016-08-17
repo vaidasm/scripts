66d02069f9cedd2d993eea90efa72b0b9fa6bf11
Vaidas Mikalauskas <vaidas.m@datadog.org>
2016-08-17 09:21:32 +0300

    zsh setup updates

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 scripts
 =======
 
-scripts
+Install
+
+```
+git clone git@github.com:vaidasm/scripts.git ~/.dotfiles
+```
+
+Run ZSH setup:
+
+```
+sh ~/.dotfiles/setups/setup-zsh.sh
+```
--- setups/setup-zsh.sh
+++ setups/setup-zsh.sh
@@ -4,4 +4,6 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
+cp -n ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
 sed 's/^\# ZSH_CUSTOM=.*$/ZSH_CUSTOM=\~\/\.dotfiles\/zsh/g' ~/.zshrc
